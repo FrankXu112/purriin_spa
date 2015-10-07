7a64a3d52477fcb8d2911c85ecfc2e6cebceb444
Frank Xu <frankxu1992@gmail.com>
2015-10-07 14:34:07 -0700

    deleted unnecessary model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926051301) do
-
-  create_table "spas", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "time"
-    t.string   "description"
-    t.integer  "price"
-    t.string   "url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
